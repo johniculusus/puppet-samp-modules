3c601a8b7e051f04e0c7274c2f50517aa3e5f147
johniculusus <johndiaz.developer@gmail.com>
2017-01-16 17:24:08 +0000

    testing git module

--- production/manifests/site.pp
+++ production/manifests/site.pp
@@ -1,6 +1,18 @@
-node 'puppetc' { # This node is not connected to HPE network / no proxy
+node 'ip-172-31-31-65.ap-southeast-1.compute.internal' { # puppetc1 instance
   include openssh::server
   include filetransferbucket::server
+}
+
+node 'ip-172-31-31-67.ap-southeast-1.compute.internal' { # puppetc2 instance
+  # Testing puppetlabs-git module from puppetforge
+
+  include git
+  git::config { 'user.name':
+    value => 'johniculusus',
+  }
+  git::config { 'user.email':
+    value => 'johndiaz.developer@gmail.com',
+  }
 }
 
 node default {
